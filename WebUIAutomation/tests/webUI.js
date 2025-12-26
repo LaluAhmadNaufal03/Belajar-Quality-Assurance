5184a2b159e507f30f1423665e84707da2f2a5a9
Lalu Ahmad Naufal <laluahmadnaufal03@gmail.com>
2025-12-26 22:50:47 +0700

    Membuat Web UI Automation menggunakan selenium, mocha, mochawesome, dan assert

--- WebUIAutomation/tests/webUI.js
+++ WebUIAutomation/tests/webUI.js
@@ -18,7 +18,7 @@ describe('Web UI Test', function(){
         //Validasi button login
         const buttonBefore = await driver.findElement(By.xpath('//*[@name="login-button"]'));
         await driver.wait(until.elementIsVisible(buttonBefore), 5000, 'Button Login harus tampil saat halaman login dibuka');
-        buttonBefore.isDisplayed(), true;
+        assert.strictEqual(await buttonBefore.isDisplayed(), true);
 
         //Input username, password, dan klik tombol login
         let InputUsername = await driver.findElement(By.id('user-name'));
@@ -40,30 +40,31 @@ describe('Web UI Test', function(){
         //Validasi tampilan halaman produk
         const productPage = await driver.wait(until.elementLocated(By.xpath('//*[@id="page_wrapper"]')), 4000);
         await driver.wait(until.elementIsVisible(productPage), 5000, 'Halaman produk harus tampil setelah login berhasil');
-        await productPage.isDisplayed(), true;
+        assert.strictEqual(await productPage.isDisplayed(), true);
         
         //Validasi tampilan elemen button sort produk
         const sortProduct = await driver.findElement(By.xpath('//*[@data-test="product-sort-container"]'));
         await driver.wait(until.elementIsVisible(sortProduct), 2000, 'Elemen button sort produk harus tampil di halaman produk');
-        sortProduct.isDisplayed(), true;
+        assert.strictEqual(await sortProduct.isDisplayed(), true);
 
         //Sort button produk dari A-Z
         const sortButton = await driver.findElement(By.xpath('//*[@data-test="product-sort-container"]'));
 
-        //Validasi produk terurut dari A-Z
-        await sortButton.getAttribute('value="az"'); ////getAttribute untuk mengambil attribute HTML dari element ini, disini yang di ambil attribute "value"
+        //Klik button sort dan pilih opsi A-Z
+        await sortButton.click(); //klik dulu button sortnya biar opsi tampil
         
         //Validasi Text option A-Z
         const optionAZ = await driver.findElement(By.xpath('//*[@value="az"]'));
         const optionAZText = await optionAZ.getText();
         assert.strictEqual(optionAZText, 'Name (A to Z)', 'Text option A-Z harus sesuai');
 
+        await optionAZ.click(); //klik option A-Z untuk mengurutkan produk dari A-Z
+
         //Validasi urutan produk A-Z
-        const productNames = await driver.findElement(By.xpath('//*[@value="az"]'));
+        const productNames = await driver.findElements(By.className('inventory_item_name')); //mengambil semua elemen nama produk yang ada di halaman produk
         let names = []; // [] adalah array kosong untuk menampung nama produk
-        for (let i = 0; i < productNames.length; i++) {
-            const name = await productNames[i].getText(); //[i] untuk mengakses elemen ke-i/ ke-0 dari array productNames, lalu di getText untuk mengambil teks dari elemen tersebut
-            names.push(name); //push untuk menambahkan nama produk ke dalam array names, push menmabahkan di akhir array, kalau menambahkan di awal array pakai unshift
+        for (let item of productNames) {
+            names.push(await item.getText()); //push untuk menambahkan nama produk ke dalam array names, push menmabahkan di akhir array, kalau menambahkan di awal array pakai unshift
         }
         const sortedNames = [...names].sort(); //[...names] untuk menyalin names dan di buat menjadi array baru, lalu di sort atau diurutkan secara alfabet, kenapa harus disalin? karena kalau langsung di sort names, maka names juga akan terubah urutannya tampilan UI awalnya.
         assert.deepStrictEqual(names, sortedNames, 'Produk harus terurut dari A-Z'); //names adalah urutan produk di tampilan UI, sedangkan sortedNames adalah urutan produk yang sudah di sort dari A-Z, jadi diharapkan keduanya sama.
